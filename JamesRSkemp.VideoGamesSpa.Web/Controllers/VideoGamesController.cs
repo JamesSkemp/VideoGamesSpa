1b6d8fffcb5217d3ba6c547b3b20a777c96575ac
James Skemp <strivinglife@gmail.com>
2013-04-27 18:26:35 -0500

    Began adding Web API functionality.
    Can now return a listing of video games.
    Also includes project file updates to include JavaScript files. Whoops.

---
+++ JamesRSkemp.VideoGamesSpa.Web/Controllers/VideoGamesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using JamesRSkemp.VideoGamesSpa.Web.Models;
+
+namespace JamesRSkemp.VideoGamesSpa.Web.Controllers
+{
+    public class VideoGamesController : ApiController
+    {
+        //
+        // GET: /VideoGames/
+
+        public VideoGame[] Get()
+        {
+			return VideoGame.GetGames();
+        }
+
+    }
+}
