45693c270ddd5027094a075fe87bddeb54691834
James Skemp <strivinglife@gmail.com>
2013-04-27 11:10:53 -0500

    Added two base views (home and profile).
    Currently no content on either.

--- JamesRSkemp.VideoGamesSpa.Web/app/app.js
+++ JamesRSkemp.VideoGamesSpa.Web/app/app.js
@@ -1,1 +1,14 @@
 ﻿var app = angular.module('videoGamesApp', []);
+// Configure routes and controllers and views associated with them.
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			controller: 'HomeController',
+			templateUrl: '/app/partials/home.html'
+		})
+		.when('/profile', {
+			controller: 'ProfileController',
+			templateUrl: '/app/partials/profile.html'
+		})
+		.otherwise({ redirectTo: '/home' });
+});
--- JamesRSkemp.VideoGamesSpa.Web/app/controllers/controllers.js
+++ JamesRSkemp.VideoGamesSpa.Web/app/controllers/controllers.js
@@ -1,1 +1,21 @@
-﻿
+﻿app.controller('HomeController', function ($scope) {
+});
+
+app.controller('ProfileController', function ($scope) {
+	init();
+
+	function init() {
+		$scope.profile = '';
+	}
+
+});
+
+app.controller('NavbarController', function ($scope, $location) {
+	$scope.getClass = function (path) {
+		if ($location.path().substr(0, path.length) == path) {
+			return true
+		} else {
+			return false;
+		}
+	}
+});
