2bc70911857e7b8318760c201484b400f981250c
James Skemp <strivinglife@gmail.com>
2013-04-28 20:15:17 -0500

    Created new site with static content.

---
+++ SimpleWebsite/app/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('videoGamesApp', []);
+// Configure routes and controllers and views associated with them.
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			controller: 'HomeController',
+			templateUrl: '/app/partials/home.html'
+		})
+		.when('/games', {
+			controller: 'GamesController',
+			templateUrl: '/app/partials/games.html'
+		})
+		.when('/profile', {
+			controller: 'ProfileController',
+			templateUrl: '/app/partials/profile.html'
+		})
+		.otherwise({ redirectTo: '/home' });
+});
